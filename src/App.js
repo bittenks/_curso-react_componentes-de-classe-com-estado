c216330620208b22f29788cc6d373df04f5226b2
Guilherme <guilhermebittencourtcorrea@gmail.com>
2021-06-15 21:06:49 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
 import logo from './logo.svg';
 import './App.css';
+import {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  constructor(props){
+    super(props);
+    
+    this.state ={
+      name:'Guilherme Bittencourt',
+      counter:0
+    };
+  }
+
+  handlePClick= () =>{
+    this.setState({name:'Gui'});
+  }
+
+  handleAClick = (event) => {
+    event.preventDefault();
+    const {counter} = this.state;
+    this.setState({counter: counter +1});
+  }
+
+  render(){
+    const {name, counter} = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p onClick={this.handlePClick} >
+            {name} {counter}
+          </p>
+          <a onClick={this.handleAClick}
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+  }
+
+
+
+// function App() {
+
 
 export default App;
